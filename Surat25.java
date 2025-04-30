049e237800fee02b570400ceb15eb6693140d5d7
NewUserWIDI <wwidi4087@gmail.com>
2025-05-01 01:37:23 +0700

    jobsheet9ALSD

---
+++ Surat25.java
@@ -0,0 +1,24 @@
+
+public class Surat25 {
+  String idSurat;
+  String namaMahasiswa;
+  String kelas;
+  char jenisIzin;
+  int durasi;
+
+  public Surat25(String idSurat, String namaMahasiswa, String kelas, char jenisIzin, int durasi) {
+    this.idSurat = idSurat;
+    this.namaMahasiswa = namaMahasiswa;
+    this.kelas = kelas;
+    this.jenisIzin = jenisIzin;
+    this.durasi = durasi;
+  }
+  public void tampilkanSurat() {
+    System.out.println("ID Surat     : " + idSurat);
+    System.out.println("Nama         : " + namaMahasiswa);
+    System.out.println("Kelas        : " + kelas);
+    System.out.println("Jenis Izin   : " + (jenisIzin == 'S' ? "Sakit" : "Izin"));
+    System.out.println("Durasi (hari): " + durasi);
+  }
+}
+
